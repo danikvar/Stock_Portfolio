c71faa5af30151a6254346f0ea2cd04ce5b0a628
Pavi200118 <59041297+Pavi200118@users.noreply.github.com>
2022-10-27 21:13:26 -0400

    Update TextGUI.java

--- src/stockData/TextGUI.java
+++ src/stockData/TextGUI.java
@@ -6,29 +6,29 @@ public class TextGUI implements TextInterface{
   public TextGUI(PrintStream out) {
     this.out = out;
   }
-  @Override
+ 
   public void showOptions() {
     //Enter name of the user
     out.println("Enter Name of user:");
   }
-  @Override
+  
   public void showOptions1() {
     //Portfolio is displayed if present.
     out.println("Portfolio:");
   }
-  @Override
+  
   public void createnew() {
     //Portfolio is created if not present already.
     out.println("New portfolio created");
   }
-  @Override
+ 
   public void addStock() {
     //Menu to either add stock or determine value on a date.
     out.println("Menu: ");
     out.println("E : Add stock");
     out.println("Q : Determine total value on a date");
   }
-  @Override
+  
   public void showOptionError() {
     //If E or Q is entered by the user, then invalid option is chosen.
     out.print("\nInvalid option. Please try again.");
@@ -39,7 +39,7 @@ public class TextGUI implements TextInterface{
     out.println("Enter stock");
     out.println("How many shares?");
   }
-  @Override
+  
   public void totalValue() {
     //Total value displayed for a particular date.
     out.println("| TICKER | DATE | Num_Shares | open | high |  low | close | volume |");
