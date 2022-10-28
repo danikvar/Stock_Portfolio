0d5bc382f7c44417fa58479cf74c2a3d1de35038
Pavi200118 <59041297+Pavi200118@users.noreply.github.com>
2022-10-27 20:49:28 -0400

    Add files via upload

---
+++ src/stockData/TextGUI.java
@@ -0,0 +1,43 @@
+import static java.lang.System.out;
+
+public class TextGUI implements TextInterface{
+  @Override
+  public void showOptions() {
+    //Enter name of the user
+    out.println("Enter Name of user:");
+  }
+  @Override
+  public void showOptions1() {
+    //Portfolio is displayed if present.
+    out.println("Portfolio:");
+  }
+  @Override
+  public void createnew() {
+    //Portfolio is created if not present already.
+    out.println("New portfolio created");
+  }
+  @Override
+  public void addStock() {
+    //Menu to either add stock or determine value on a date.
+    out.println("Menu: ");
+    out.println("E : Add stock");
+    out.println("Q : Determine total value on a date");
+  }
+  @Override
+  public void showOptionError() {
+    //If E or Q is entered by the user, then invalid option is chosen.
+    out.print("\nInvalid option. Please try again.");
+  }
+  @Override
+  public void addStockdetails() {
+    //Data required to add stock.
+    out.println("Enter stock");
+    out.println("How many shares?");
+  }
+  @Override
+  public void totalValue() {
+    //Total value displayed for a particular date.
+    out.println("| TICKER | DATE | Num_Shares | open | high |  low | close | volume |");
+  }
+
+}
---
+++ src/stockData/TextInterface.java
@@ -0,0 +1,16 @@
+public interface TextInterface {
+
+  void showOptions();
+
+  void showOptions1();
+
+  void createnew();
+
+  void addStock();
+
+  void addStockdetails();
+
+  void totalValue();
+
+  void showOptionError();
+}
